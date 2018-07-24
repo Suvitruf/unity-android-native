a8750f2e8da0ae73fa0ada245806382145c3964e
Suvitruf <suvitruf@gmail.com>
2018-07-24 21:10:04 +0300

    ServiceInfo

--- Assets/UnityAndroidNative/Android/Content/Pm/ResolveInfo.cs
+++ Assets/UnityAndroidNative/Android/Content/Pm/ResolveInfo.cs
@@ -4,14 +4,24 @@ using Object = UnityAndroidNative.Java.Lang.Object;
 
 namespace UnityAndroidNative.Android.Content.Pm {
     public class ResolveInfo : Object {
-        private ActivityInfo mActivityInfo;
 
+        /// <summary>
+        /// The activity or broadcast receiver that corresponds to this resolution
+        /// match, if this resolution is for an activity or broadcast receiver.
+        /// Exactly one of <see cref="ActivityInfo"/>, <see cref="ServiceInfo"/>, or
+        /// {@link #providerInfo} will be non-null.
+        /// </summary>
         public ActivityInfo ActivityInfo {
-            get {
-                if (mActivityInfo == null)
-                    mActivityInfo = Get<ActivityInfo>("activityInfo");
-                return mActivityInfo;
-            }
+            get { return Get<ActivityInfo>("activityInfo"); }
+        }
+
+        /// <summary>
+        /// The service that corresponds to this resolution match, if this resolution
+        /// is for a service. Exactly one of <see cref="ActivityInfo"/>,
+        /// <see cref="ServiceInfo"/>, or {@link #providerInfo} will be non-null.
+        /// </summary>
+        public ServiceInfo ServiceInfo {
+            get { return Get<ServiceInfo>("serviceInfo"); }
         }
 
         public int Icon {
@@ -21,9 +31,37 @@ namespace UnityAndroidNative.Android.Content.Pm {
         internal ResolveInfo(IntPtr obj) : base(obj) {
         }
 
+        public ResolveInfo(ResolveInfo orig) : base(orig) {
+        }
+        /// <summary>
+        /// Retrieve the current textual label associated with this resolution.  
+        /// This will call back on the given PackageManager to load the label from the application.
+        /// </summary>
+        /// <param name="pm">A PackageManager from which the label can be loaded; 
+        /// usually the PackageManager from which you originally retrieved this item.</param>
+        /// <returns>Returns a CharSequence containing the resolutions's label.  
+        /// If the item does not have a label, its name is returned.</returns>
         public string LoadLabel(PackageManager pm) {
             return Call<string>("loadLabel", pm);
-        }       
+        }
+
+        /// <summary>
+        /// Return the icon resource identifier to use for this match.If the
+        /// match defines an icon, that is used; else if the activity defines
+        /// an icon, that is used; else, the application icon is used.
+        /// </summary>
+        /// <returns>The icon associated with this match.</returns>
+        public int GetIconResource() {
+            return Call<int>("getIconResource");
+        }
+
+        public ComponentInfo GetComponentInfo() {
+            return Call<ComponentInfo>("getComponentInfo");
+        }
+
+        public override string ToString() {
+            return Call<string>("toString");
+        }
     }
 }
 #endif
---
+++ Assets/UnityAndroidNative/Android/Content/Pm/ServiceInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Object = UnityAndroidNative.Java.Lang.Object;
+
+namespace UnityAndroidNative.Android.Content.Pm {
+    public class ServiceInfo : Object {
+
+        /// <summary>
+        /// Optional name of a permission required to be able to access this Service.  From the "permission" attribute.
+        /// </summary>
+        public string Permission {
+            get { return Get<string>("permission"); }
+        }
+
+        public int Flags {
+            get { return Get<int>("flags"); }
+        }
+
+        /// <summary>
+        /// Bit in <see cref="Flags"/>: If set, the service will automatically be
+        /// stopped by the system if the user removes a task that is rooted
+        /// in one of the application's activities. 
+        /// </summary>
+        public const int FLAG_STOP_WITH_TASK = 0x0001;
+
+        /// <summary>
+        /// Bit in <see cref="Flags"/>: If set, the service will run in its own isolated process.
+        /// </summary>
+        public const int FLAG_ISOLATED_PROCESS = 0x0002;
+
+        /// <summary>
+        /// Bit in <see cref="Flags"/>: If set, the service can be bound and run in the
+        /// calling application's package, rather than the package in which it is
+        /// declared.
+        /// </summary>
+        public const int FLAG_EXTERNAL_SERVICE = 0x0004;
+
+        /// <summary>
+        /// Bit in <see cref="Flags"/> indicating if the service is visible to ephemeral applications.
+        /// </summary>
+        public const int FLAG_VISIBLE_TO_INSTANT_APP = 0x100000;
+
+        /// <summary>
+        /// Bit in <see cref="Flags"/>: If set, a single instance of the service will
+        /// run for all users on the device.
+        /// </summary>
+        public const int FLAG_SINGLE_USER = 0x40000000;
+
+        internal ServiceInfo(IntPtr obj) : base(obj) {
+        }
+
+        public ServiceInfo() : base() {
+        }
+
+        public ServiceInfo(ServiceInfo orig) : base(orig) {
+        }
+
+        public override string ToString() {
+            return Call<string>("toString");
+        }
+    }
+}
